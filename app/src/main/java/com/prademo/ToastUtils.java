93b0acd5914b95c956041f0e77b4456d047c2f98
Administrator <229008199@qq.com>
2018-01-04 21:08:14 +0800

    Initial commit2222

---
+++ app/src/main/java/com/prademo/ToastUtils.java
@@ -0,0 +1,14 @@
+package com.prademo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by 谭杰栖 on 2018/1/4.
+ */
+
+public class ToastUtils {
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+}
